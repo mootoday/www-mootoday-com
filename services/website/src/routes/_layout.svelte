79184876299ba18b2c8bc437c1501498ba00a0f3
Mike Nikles <mikenikles@gmail.com>
2019-11-16 12:17:55 +0000

    Remove Sapper template files.

--- services/website/src/routes/_layout.svelte
+++ services/website/src/routes/_layout.svelte
@@ -1,24 +1,13 @@
 <script>
   import TailwindBase from '../tailwind-base.svelte'
-	import Nav from '../components/Nav.svelte';
 
 	export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 </style>
 
 <TailwindBase />
-
-<Nav {segment}/>
 
 <main>
 	<slot></slot>
