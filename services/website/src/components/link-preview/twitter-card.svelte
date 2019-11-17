b6df5f08128d57041c0922b513cee2160b9321f5
Mike Nikles <mikenikles@gmail.com>
2019-11-17 18:31:30 +0000

    Add og:image and sort meta tags alphabetically.

--- services/website/src/components/link-preview/twitter-card.svelte
+++ services/website/src/components/link-preview/twitter-card.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="{post.twitter_title || post.meta_title}">
-<meta name="twitter:description" content="{post.twitter_description || post.meta_description}">
-<meta name="twitter:url" content="{post.url}">
 <meta name="twitter:creator" content="@mikenikles" />
+<meta name="twitter:description" content="{post.twitter_description || post.meta_description}">
 <meta name="twitter:image" content="{post.twitter_image || post.feature_image}">
 <meta name="twitter:image:alt" content="{post.twitter_title || post.meta_title}">
+<meta name="twitter:title" content="{post.twitter_title || post.meta_title}">
+<meta name="twitter:url" content="{post.url}">
