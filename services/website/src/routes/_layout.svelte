144ea856a878406317ecb9feaf1eaa12f11f6420
Mike Nikles <mikenikles@gmail.com>
2019-11-28 20:30:52 +0000

    Enable media queries.

--- services/website/src/routes/_layout.svelte
+++ services/website/src/routes/_layout.svelte
@@ -4,42 +4,42 @@
 </script>
 
 <style>
-  /* .small-screen { */
-  /* @apply max-w-6xl */
-  /* m-auto */
-  /* antialiased */
-  /* text-gray-900 */
-  /* flex-wrap */
-  /* hidden */
-  /* md:flex */
-  /* sm:block; */
-  /* @media (min-width: 640px) { */
-  /* display: block; */
-  /* } */
-
-  /* @screen sm {
-      @apply text-red-600;
-    } */
-  /* } */
-
   .small-screen {
-    @apply sm:text-left;
+    @apply max-w-6xl
+    m-auto
+    antialiased
+    text-gray-900
+    flex-wrap;
+  }
+
+  .large-screen {
+    @apply hidden;
   }
 
   .right-side {
     @apply w-3/4 
 		ml-72;
   }
+
+  @screen md {
+    .large-screen {
+      @apply block;
+    }
+
+    .small-screen {
+      @apply hidden;
+    }
+  }
 </style>
 
 <TailwindBase />
-<main class="small-screen">
+<main class="large-screen">
   <LeftSideOverview />
   <div class="right-side">
     <slot />
   </div>
 </main>
-<!-- 
-<main>
+
+<main class="small-screen">
   <slot />
-</main> -->
+</main>
