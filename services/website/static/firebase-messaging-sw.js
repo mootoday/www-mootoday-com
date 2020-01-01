f35d199662cf0b4e3b3deecd4000fc0a94c69bce
Mike <mikenikles@gmail.com>
2020-01-01 19:52:27 +0000

    Use the correct path for Firebase files.

--- services/website/static/firebase-messaging-sw.js
+++ services/website/static/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts("/__/7.6.0/firebase-app.js");
-importScripts("/__/7.6.0/firebase-messaging.js");
-importScripts("/__/7.6.0/init.js");
+importScripts("/__/firebase/7.6.0/firebase-app.js");
+importScripts("/__/firebase/7.6.0/firebase-messaging.js");
+importScripts("/__/firebase/7.6.0/init.js");
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
