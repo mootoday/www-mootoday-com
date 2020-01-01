86a1254243f2106f034d16e0f44f07d1db5fa36b
Mike <mikenikles@gmail.com>
2020-01-01 12:36:11 +0000

    Allow visitors to leave comments by using Commento.

---
+++ services/website/src/components/comments.svelte
@@ -0,0 +1,18 @@
+<style>
+  h2 {
+    @apply text-2xl
+    mt-4
+		text-brown-800
+		font-bold
+    leading-tight;
+  }
+</style>
+
+<svelte:head>
+  <script src="https://cdn.commento.io/js/commento.js">
+
+  </script>
+</svelte:head>
+
+<h2>Comments, Feedback, Thoughts</h2>
+<div id="commento" />
