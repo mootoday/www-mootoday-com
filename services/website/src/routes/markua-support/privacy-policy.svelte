73eb769774273def1f53c6a29c3f9ec046fc16c6
Mike Nikles <mikenikles@gmail.com>
2020-03-22 13:49:18 +0000

    Add Markua Support pages.

---
+++ services/website/src/routes/markua-support/privacy-policy.svelte
@@ -0,0 +1,21 @@
+<h1>Markua Support - Privacy Policy</h1>
+
+<p>
+  The Markua Support Google Docd add-on does not collect or distribute any
+  information about you or the documents it converts.
+</p>
+
+<h2>Required permissions</h2>
+
+<p>
+  Markua Support Google Docd add-on will ask for permission to access the current Doc (to convert
+  it and insert Markua snippets). Markua Support Google Docd add-on reads the document content and structure; it inserts snippets at the cursor's position if you instruct it to do so.
+</p>
+
+<p>
+  Markua Support Google Docd add-on will also ask for permission to display a sidebar. It uses
+  the sidebar to display action buttons and options as well as the
+  converted Markua. It allows you to copy the Markua to your clipboard.
+</p>
+
+<p>No other permissions are required.</p>
