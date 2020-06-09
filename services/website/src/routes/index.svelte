48666722a9e2dab08784a2db1621d129642b8c22
Mike Nikles <mikenikles@gmail.com>
2020-06-09 19:33:54 +0000

    Add new homepage content.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -1,46 +1,7 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Mike's Personal Website</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Welcome to Mike's website, v2.0</h1>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>This is being developed as you read it.</p>
