93d40321164ee37089e39043ba067c2117d5d631
Mike Nikles <mikenikles@gmail.com>
2020-06-12 11:15:34 +0000

    Add an additional commit.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -5,4 +5,4 @@
 <h1>Welcome to Mike's website, v2.0</h1>
 
 <p>This is being developed as you read it.</p>
-<p>We are testing a preview deployment on Vercel.com</p>
+<p>We are testing a preview deployment on Vercel.com. This is deployment 2.</p>
