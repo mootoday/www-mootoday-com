c4925cd44cf7ac9a84e8c5f905b051ce02916829
Mike Nikles <mikenikles@gmail.com>
2020-06-15 15:58:25 +0000

    Add a generator to create new blog posts.

---
+++ generators/blog-post.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+module.exports = (plop) => {
+  return {
+    description: "Generate a new blog post.",
+    prompts: [
+      {
+        type: "input",
+        name: "blogPostTitle",
+        message:
+          "What's the title? (e.g. How to deploy a Sapper app to Cloud Run)",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path:
+          "../services/website/src/blog-posts/{{today}}-{{dashCase blogPostTitle}}/index.svx",
+        templateFile: "../templates/blog-post/index.svx.hbs",
+      },
+      {
+        type: "add",
+        path:
+          "../services/website/src/routes/blog/{{dashCase blogPostTitle}}.svelte",
+        templateFile: "../templates/blog-post/page.svelte.hbs",
+      },
+      {
+        type: "add",
+        path:
+          "../services/website/statuc/blog-posts/{{today}}-{{dashCase blogPostTitle}}/cover.jpg",
+        templateFile: "../templates/blog-post/cover.jpg",
+      },
+    ],
+  };
+};
