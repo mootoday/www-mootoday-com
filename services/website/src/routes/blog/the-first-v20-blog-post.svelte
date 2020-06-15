6514fef5a7d0fdd7c90387e5dd3507da2f356eec
Mike Nikles <mikenikles@gmail.com>
2020-06-15 11:59:37 +0000

    Introduce a BaseBlogPosts component to abstract common code for all blog posts.

--- services/website/src/routes/blog/the-first-v20-blog-post.svelte
+++ services/website/src/routes/blog/the-first-v20-blog-post.svelte
@@ -1,5 +1,8 @@
 <script>
-  import Post from "../../blog-posts/2020-06-14-the-first-v20-blog-post/index.svx";
+  import BaseBlogPost from "../../components/base-blog-post.svelte";
+  import BlogPost, { metadata } from "../../blog-posts/2020-06-14-the-first-v20-blog-post/index.svx";
 </script>
 
-<Post />
+<BaseBlogPost title={metadata.title}>
+  <BlogPost />
+</BaseBlogPost>
