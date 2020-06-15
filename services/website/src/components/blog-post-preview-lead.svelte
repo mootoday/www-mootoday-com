2333dc7665dcec286c3342834b34ee9d3f70eb40
Mike Nikles <mikenikles@gmail.com>
2020-06-15 20:51:17 +0000

    WIP: Styled homepage with lorem ipsum content.

---
+++ services/website/src/components/blog-post-preview-lead.svelte
@@ -0,0 +1,44 @@
+        <div class="flex h-full bg-white rounded overflow-hidden shadow-lg">
+          <a
+            href="post.html"
+            class="flex flex-wrap no-underline hover:no-underline">
+            <div class="w-full md:w-2/3 rounded-t">
+              <img
+                src="https://source.unsplash.com/collection/494263/800x600"
+                class="h-full w-full shadow"
+                alt="TODO" />
+            </div>
+
+            <div class="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
+              <div
+                class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden
+                shadow-lg">
+                <p class="w-full text-gray-900 text-xs md:text-sm pt-6 px-6">
+                  GETTING STARTED
+                </p>
+                <h2 class="w-full font-bold text-xl text-gray-900 px-6">
+                  👋 Welcome fellow Tailwind CSS and Ghost fan
+                </h2>
+                <p class="text-gray-900 font-serif text-base px-6 mb-5">
+                  This starter template is an attempt to replicate the default
+                  Ghost theme "Casper" using Tailwind CSS and vanilla
+                  Javascript.
+                </p>
+              </div>
+
+              <div
+                class="flex-none mt-auto bg-white rounded-b rounded-t-none
+                overflow-hidden shadow-lg p-6">
+                <div class="flex items-center justify-between">
+                  <img
+                    class="w-8 h-8 rounded-full mr-4 avatar"
+                    data-tippy-content="Author Name"
+                    src="images/profile-pic.jpg"
+                    alt="Avatar of Author" />
+                  <p class="text-gray-900 text-xs md:text-sm">1 MIN READ</p>
+                </div>
+              </div>
+            </div>
+
+          </a>
+        </div>
---
+++ services/website/src/components/blog-post-preview.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let col;
+</script>
+
+<div class="w-full md:{col} p-6 flex flex-col flex-grow flex-shrink">
+  <div
+    class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+    <a href="tbd.html" class="flex flex-wrap no-underline hover:no-underline">
+      <img
+        src="https://source.unsplash.com/collection/225/800x600"
+        class="h-64 w-full rounded-t pb-6 object-cover"
+        alt="TODO" />
+      <p class="w-full text-gray-900 text-xs md:text-sm px-6">
+        GETTING STARTED
+      </p>
+      <div class="w-full font-bold text-xl text-gray-900 px-6">
+        Lorem ipsum dolor sit amet.
+      </div>
+      <p class="text-gray-900 font-serif text-base px-6 mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
+        ipsum eu nunc commodo posuere et sit amet ligula.
+      </p>
+    </a>
+  </div>
+  <div
+    class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden
+    shadow-lg p-6">
+    <div class="flex items-center justify-between">
+      <img
+        class="w-8 h-8 rounded-full mr-4 avatar"
+        data-tippy-content="Author Name"
+        src="images/profile-pic.jpg"
+        alt="Avatar of Author" />
+      <p class="text-gray-900 text-xs md:text-sm">1 MIN READ</p>
+    </div>
+  </div>
+</div>
