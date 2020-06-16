e2b60ffb86520d1788860999ae3607d73b6cb590
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:38:04 +0000

    Add blog post 'Why I moved from React to Svelte and others will follow'

---
+++ services/website/src/routes/blog/why-i-moved-from-react-to-svelte-and-others-will-follow/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2019-12-26-why-i-moved-from-react-to-svelte-and-others-will-follow/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
