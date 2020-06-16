23ae0a140d1ef2d6093cc81e387d5594064b0a72
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:04:20 +0000

    Add blog post 'Firebase Hosting for static assets of a Sapper web app on Cloud Run'

---
+++ services/website/src/routes/blog/firebase-hosting-for-static-assets-of-a-sapper-web-app-on-cloud-run/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-04-28-firebase-hosting-for-static-assets-of-a-sapper-web-app-on-cloud-run/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
