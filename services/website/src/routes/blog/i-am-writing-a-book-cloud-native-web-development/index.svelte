9c37c6bfb7caff4536e168a50970f5d9adccda57
Mike Nikles <mikenikles@gmail.com>
2020-06-16 17:44:46 +0000

    Add blog post 'I am writing a book: Cloud Native Web Development'

---
+++ services/website/src/routes/blog/i-am-writing-a-book-cloud-native-web-development/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-05-10-i-am-writing-a-book-cloud-native-web-development/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
