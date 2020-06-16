c299b0a398fef752820d91cb93323ec3aaf36110
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:33:29 +0000

    Add blog post 'How to use Svelte for your Google Docs add-on'

---
+++ services/website/src/routes/blog/how-to-use-svelte-for-your-google-docs-add-on/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-03-28-how-to-use-svelte-for-your-google-docs-add-on/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
