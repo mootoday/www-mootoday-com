2028c5f09af6db9ec2f39a0c53bf853606f3dcd9
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:47:18 +0000

    Add blog post 'A (mostly) automated release process'

---
+++ services/website/src/routes/blog/a-mostly-automated-release-process/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2017-12-26-a-mostly-automated-release-process/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
