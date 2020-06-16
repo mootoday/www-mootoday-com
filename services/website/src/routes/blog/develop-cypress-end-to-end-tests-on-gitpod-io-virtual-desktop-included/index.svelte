52b1e0f6909ceacd7f6f3b2dad05be3d17855426
Mike Nikles <mikenikles@gmail.com>
2020-06-16 17:59:20 +0000

    Add blog post '<div class=post-content>'

---
+++ services/website/src/routes/blog/develop-cypress-end-to-end-tests-on-gitpod-io-virtual-desktop-included/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-05-02-develop-cypress-end-to-end-tests-on-gitpod-io-virtual-desktop-included/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
