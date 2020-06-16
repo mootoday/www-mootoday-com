2931c035333a6e0947036a1c08633c7f01ac5f4a
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:35:51 +0000

    Add blog post 'Add a commenting platform to a Svelte app'

---
+++ services/website/src/routes/blog/add-a-commenting-platform-to-a-svelte-app/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-01-01-add-a-commenting-platform-to-a-svelte-app/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
