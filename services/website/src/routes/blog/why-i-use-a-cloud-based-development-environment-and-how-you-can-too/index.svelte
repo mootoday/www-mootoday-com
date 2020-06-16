82728d7cdb3cdbb0c9d8b8e401f7058a40d7919f
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:30:06 +0000

    Add blog post 'Why I use a cloud-based development environment and how you can too'

---
+++ services/website/src/routes/blog/why-i-use-a-cloud-based-development-environment-and-how-you-can-too/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-03-31-why-i-use-a-cloud-based-development-environment-and-how-you-can-too/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
