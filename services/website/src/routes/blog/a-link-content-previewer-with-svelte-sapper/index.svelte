a528d35fa69ab07b335796f2bb6b017fdf4d13cc
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:20:46 +0000

    Add blog post 'A Link Content Previewer with Svelte & Sapper'

---
+++ services/website/src/routes/blog/a-link-content-previewer-with-svelte-sapper/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-04-18-a-link-content-previewer-with-svelte-sapper/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
