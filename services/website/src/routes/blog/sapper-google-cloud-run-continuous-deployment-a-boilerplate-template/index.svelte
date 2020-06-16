fce58e42aa80206d04c6e0b5dccfd4ca5a883efc
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:11:21 +0000

    Revert "Revert "Add blog post '<div class=post-content>'""
    This reverts commit 1ea48fb71c5b8f55abbe04d8a5f677336f0f9b8e.

---
+++ services/website/src/routes/blog/sapper-google-cloud-run-continuous-deployment-a-boilerplate-template/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-04-28-sapper-google-cloud-run-continuous-deployment-a-boilerplate-template/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
