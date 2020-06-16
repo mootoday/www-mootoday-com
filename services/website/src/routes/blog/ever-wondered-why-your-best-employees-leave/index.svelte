13802f232c111e6e97e505e1120a1828988f75b1
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:51:47 +0000

    Add blog post 'Ever wondered why your best employees leave'

---
+++ services/website/src/routes/blog/ever-wondered-why-your-best-employees-leave/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2016-04-11-ever-wondered-why-your-best-employees-leave/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
