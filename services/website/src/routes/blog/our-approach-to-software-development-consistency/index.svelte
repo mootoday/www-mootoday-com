9605898839abfb2b1d9ca60b0d96f4f8c2b1acec
Mike Nikles <mikenikles@gmail.com>
2020-06-16 18:42:05 +0000

    Add blog post 'Our approach to software development consistency'

---
+++ services/website/src/routes/blog/our-approach-to-software-development-consistency/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2018-01-03-our-approach-to-software-development-consistency/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
