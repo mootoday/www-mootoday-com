830908f39bbf65abb227d6ed732884567aaffbb7
Mike Nikles <mikenikles@gmail.com>
2020-06-16 14:37:45 +0000

    Add the 'Deploy a static Sapper app with Deno on Cloud Run' blog post.

---
+++ services/website/src/routes/blog/deploy-a-static-sapper-app-with-deno-on-cloud-run/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-05-14-deploy-a-static-sapper-app-with-deno-on-cloud-run/index.svx";
+</script>
+
+<BlogPostWrapper {metadata}>
+  <BlogPost />
+</BlogPostWrapper>
