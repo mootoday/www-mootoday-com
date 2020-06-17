c3ca0791e81a4f79f21831ecc43b0a4c0844cebe
Mike Nikles <mikenikles@gmail.com>
2020-06-17 20:17:20 +0000

    Remove a debug log statement.

--- services/website/rollup.config.js
+++ services/website/rollup.config.js
@@ -15,8 +15,6 @@ const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-console.log(rehypePicture);
-
 const onwarn = (warning, onwarn) =>
   (warning.code === "CIRCULAR_DEPENDENCY" &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
