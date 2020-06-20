ff92bd5bdc9fcabf5edc5f2593d229dcff35067a
Mike Nikles <mikenikles@gmail.com>
2020-06-20 15:36:23 +0000

    Add a top nav bar; use IntersectionObserver to trigger its display.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -12,13 +12,33 @@
   import BlogPostPreview from "../components/blog-post/previews/index.svelte";
   import BlogPostPreviewLead from "../components/blog-post/previews/lead.svelte";
   import Footer from "../components/footer.svelte";
-  import HomepageNav from "../components/homepage-nav.svelte";
+  import NavHeader from "../components/nav/header.svelte";
   import Subscribe from "../components/subscribe.svelte";
 
   export let posts;
 
   // Without cloning the posts, it is an empty array when hydration kicks in.
   const postsArray = [...posts];
+
+  let isShowHeader = false;
+
+  const homepageNavAction = node => {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          isShowHeader = !entry.isIntersecting;
+        });
+      }
+    );
+
+    observer.observe(node);
+
+    return {
+      destroy() {
+        observer.disconnect();
+      }
+    };
+  };
 </script>
 
 <svelte:head>
@@ -26,11 +46,15 @@
 </svelte:head>
 
 <div class="bg-gray-200 font-sans leading-normal tracking-normal">
+  {#if isShowHeader}
+    <NavHeader />
+  {/if}
   <div
     class="w-full m-0 p-0 bg-cover bg-bottom"
     style="background-image:url('images/cover.jpg'); height: 60vh;
     max-height:460px;">
     <div
+      use:homepageNavAction
       class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
       <h1 class="text-white font-extrabold text-3xl md:text-5xl">
         👋 Mike Nikles
@@ -41,12 +65,10 @@
 
   <div class="container px-4 md:px-0 max-w-6xl mx-auto -mt-32">
     <div class="mx-0 sm:mx-6">
-      <HomepageNav />
-
       <div
         class="bg-gray-200 w-full text-xl md:text-2xl text-gray-800
         leading-normal rounded-t">
-        <BlogPostPreviewLead post={postsArray.shift()}/>
+        <BlogPostPreviewLead post={postsArray.shift()} />
         <div class="flex flex-wrap justify-between py-12 md:-mx-6">
           {#each postsArray as post}
             <BlogPostPreview {post} />
