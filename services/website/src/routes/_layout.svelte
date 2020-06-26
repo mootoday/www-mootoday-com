6963b1590b77f46135c2416809845ff10c4f7dfb
Mike Nikles <mikenikles@gmail.com>
2020-06-26 08:56:17 +0000

    Remove the unused segment prop.

--- services/website/src/routes/_layout.svelte
+++ services/website/src/routes/_layout.svelte
@@ -2,13 +2,11 @@
   import Header from "../components/header/index.svelte";
   import TailwindCSS from "../components/tailwindcss.svelte";
   import WebAnalytics from "../components/web-analytics.svelte";
-
-  export let segment;
 </script>
 
 <TailwindCSS />
 
-<Header {segment} />
+<Header />
 <main>
 	<slot></slot>
 </main>
