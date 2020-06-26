95f4994417ede4cd0558e1a48c523859dd59fa46
Mike Nikles <mikenikles@gmail.com>
2020-06-26 08:56:40 +0000

    Add the new CNWD book landing page and link to it.

--- services/website/src/components/header/index.svelte
+++ services/website/src/components/header/index.svelte
@@ -28,7 +28,7 @@
       {
         label: "Cloud Native Web Development",
         description: "A hands-on guidebook: From source code to production.",
-        href: "https://www.gum.co/cloud-native-web-development",
+        href: "/cloud-native-web-development",
         svgPath:
           "M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
       },
