ef49081102e1dd298ecb8ccae613ee848dc5ed44
Mike Nikles <mikenikles@gmail.com>
2020-06-27 23:47:41 +0000

    Define the stats.

--- services/website/src/components/cloud-native-web-development/stats.svelte
+++ services/website/src/components/cloud-native-web-development/stats.svelte
@@ -23,10 +23,10 @@
   <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-4xl mx-auto text-center">
       <h3 class="text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
-        Trusted by developers from over 80 planets
+        A book, source code & a community
       </h3>
       <p class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.
+        Be part of a a community with other readers. Review & discuss source code. Ask questions.
       </p>
     </div>
   </div>
@@ -38,26 +38,26 @@
           <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
             <div class="flex flex-col border-b p-6 text-center sm:border-0 sm:border-r">
               <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500" id="item-1">
-                Chapters
+                Pull Requests
               </dt>
               <dd class="number order-1 text-5xl leading-none font-extrabold" aria-describedby="item-1">
-                100%
+                19
               </dd>
             </div>
             <div class="flex flex-col border-t border-b p-6 text-center sm:border-0 sm:border-l sm:border-r">
               <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                Delivery
+                Chapters
               </dt>
               <dd class="number order-1 text-5xl leading-none font-extrabold">
-                24/7
+                32
               </dd>
             </div>
             <div class="flex flex-col border-t p-6 text-center sm:border-0 sm:border-l">
               <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                Calories
+                Pages
               </dt>
               <dd class="number order-1 text-5xl leading-none font-extrabold">
-                100k
+                212
               </dd>
             </div>
           </dl>
