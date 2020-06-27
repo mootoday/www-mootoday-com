d81e50fe153b63d89a639b25d60637ef8428ee5f
Mike Nikles <mikenikles@gmail.com>
2020-06-27 23:47:26 +0000

    Animate the Buy Now button relocation.

--- services/website/src/components/cloud-native-web-development/buy-button.svelte
+++ services/website/src/components/cloud-native-web-development/buy-button.svelte
@@ -13,27 +13,20 @@
     font-weight: 500;
     font-style: normal;
     right: 1rem;
+    transform: translate(0, 5vh)
   }
 
   span.top {
-    top: 1rem;
+    transform: translate(0, 5vh)
   }
 
   span.bottom {
-    bottom: 1rem;
+    transform: translate(0, 90vh)
   }
 
   @screen lg {
     span {
       right: 4rem;
-    }
-
-    span.bottom {
-      bottom: 4rem;
-    }
-
-    span.top {
-      top: 4rem;
     }
   }
 </style>
@@ -41,7 +34,7 @@
 <span
   class:top={isPositionTop}
   class:bottom={isPositionBottom}
-  class="fixed block z-20 py-2 px-4 md:py-3 md:px-6 rounded-lg
+  class="fixed transform duration-500 ease-in-out z-20 py-2 px-4 md:py-3 md:px-6 rounded-lg
   text-xl tracking-widest">
   <a href="https://gumroad.com/l/cloud-native-web-development">Buy Now</a>
 </span>
