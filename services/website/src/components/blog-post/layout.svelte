4425361f809ffb8b1fd0561eecbe6d9cd23ad481
Mike Nikles <mikenikles@gmail.com>
2020-11-11 23:58:38 +0000

    Refactor all blog posts into proper pages.

---
+++ services/website/src/components/blog-post/layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import BlogPostWrapper from "./wrapper.svelte";
+
+  export let title;
+  export let slug;
+  export let summary;
+  export let createdAt;
+  export let tags;
+  export let layout;
+
+  const post = {
+    metadata: {
+      title,
+      slug,
+      summary,
+      createdAt,
+      tags,
+      layout,
+    }
+  };
+</script>
+
+<BlogPostWrapper {post}>
+  <slot />
+</BlogPostWrapper>
