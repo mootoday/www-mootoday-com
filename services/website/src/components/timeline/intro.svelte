c8fdf4e6d3b8e663cbe8f8c4669c82698f50b76a
Mike Nikles <mikenikles@gmail.com>
2020-12-03 02:04:09 +0000

    Add the intro content.

--- services/website/src/components/timeline/intro.svelte
+++ services/website/src/components/timeline/intro.svelte
@@ -1,3 +1,5 @@
 <div>
-  TODO: Render intro here
+  <h1>A story, my story...</h1>
+  <p>"Always learn, always grow" - A summary of my professional achievements.</p>
+  <p>Below is an ever-growing timelime of what I work on.</p>
 </div>
