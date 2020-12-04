46fcdbc593481c3d14374cf6d81aa305091fff13
Mike Nikles <mikenikles@gmail.com>
2020-12-04 03:55:09 +0000

    Spell timeline properly.

--- services/website/src/components/timeline/intro.svelte
+++ services/website/src/components/timeline/intro.svelte
@@ -1,5 +1,5 @@
 <div class="text-center">
   <h1 class="py-5 text-4xl md:py-10 md:text-6xl">A story, my story...</h1>
   <p class="pt-4 px-2 text-xl md:text-4xl"><span class="font-semibold italic">"Always learn, always grow"</span> - A summary of my professional achievements.</p>
-  <p class="pt-2 text-xl md:text-4xl">Below is an ever-growing timelime of what I work on.</p>
+  <p class="pt-2 text-xl md:text-4xl">Below is an ever-growing timeline of what I work on.</p>
 </div>
