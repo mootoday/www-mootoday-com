cd110039d0bb6d93d6a46ab912b7d71361556e84
Mike Nikles <mikenikles@gmail.com>
2020-12-04 14:17:56 +0000

    Silence an expected Sapper warning.

--- services/website/rollup.config.js
+++ services/website/rollup.config.js
@@ -16,6 +16,7 @@ const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
+  (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
   (warning.code === "CIRCULAR_DEPENDENCY" &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
   onwarn(warning);
