ef8dcb9695e9af16add257a0bef469db928fa2ca
Mike Nikles <mikenikles@gmail.com>
2020-12-05 16:45:25 +0000

    Write tests to make sure the entire timeline renders.

--- services/website/cypress/integration/spec.js
+++ services/website/cypress/integration/spec.js
@@ -1,9 +1,24 @@
-describe("Sapper template app", () => {
+const timelineRaw = require("../../src/timeline-raw");
+
+describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
   it("has the correct <h1>", () => {
     cy.contains("h1", "A story, my story...");
+  });
+});
+
+describe("Homepage - Timeline", () => {
+  before(() => {
+    cy.visit("/");
+  });
+
+  timelineRaw.forEach((entry) => {
+    it(`it displays the ${entry.milestone} timeline entry`, () => {
+      cy.contains("p", entry.milestone);
+      cy.get(`a[href="${entry.link}"]`).contains(entry.cta);
+    });
   });
 });
