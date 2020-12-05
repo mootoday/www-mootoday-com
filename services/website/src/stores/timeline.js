809335a4b31fcb4169382babaed49805face7f98
Mike Nikles <mikenikles@gmail.com>
2020-12-05 00:46:20 +0000

    Extract the raw timeline to it's own file for better maintainability.

--- services/website/src/stores/timeline.js
+++ services/website/src/stores/timeline.js
@@ -1,55 +1,6 @@
 import { derived, readable, writable } from "svelte/store";
+import rawTimeline from "../timeline-raw";
 import search from "./search";
-
-const rawTimeline = [
-  {
-    timestamp: 1606937475381,
-    label: "blog",
-    milestone: "Introduced the timeline view",
-    link: "/blog/introducing-the-timeline-view",
-  },
-  {
-    timestamp: 1606923112654,
-    label: "project",
-    milestone: "10 active users of Your Analytics",
-    cta: "Check out Your Analytics",
-    link: "https://www.your-analytics.org",
-  },
-  {
-    timestamp: 1606923102654,
-    label: "blog",
-    milestone: "Why I moved from React to Svelte and you should too",
-    link: "/blog/introducing-the-timeline-view",
-  },
-  {
-    timestamp: 1606921112654,
-    label: "twitter",
-    milestone: "705 Followers 🎉",
-    cta: "Check out my tweets",
-    link: "/blog/introducing-the-timeline-view",
-  },
-  {
-    timestamp: 1577059200000,
-    label: "twitter",
-    milestone: "700 Followers 🎉",
-    cta: "Check out my tweets",
-    link: "/blog/introducing-the-timeline-view",
-  },
-  {
-    timestamp: 1577059100000,
-    label: "twitter",
-    milestone: "699 Followers 🎉",
-    cta: "Check out my tweets",
-    link: "/blog/introducing-the-timeline-view",
-  },
-  {
-    timestamp: 1526083200000,
-    label: "youtube",
-    milestone: "20 Subscribers 🎉",
-    cta: "Check out my videos",
-    link: "https://www.youtube.com",
-  },
-];
 
 const timeline = readable(rawTimeline);
 
@@ -103,7 +54,6 @@ export const timelineEntries = derived(
     const entriesReadyForDisplay = [];
     yearsWithEntries.forEach((entries, year) => {
       entries.forEach((entry) => entriesReadyForDisplay.push(entry));
-      // entriesReadyForDisplay.push(year);
 
       // At this point in the forEach loop, the latest element is the first of the currently processed year.
       // We add the `year` property to display the year in the timeline view after this specific `entry`.
---
+++ services/website/src/timeline-raw.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    timestamp: 1606937475381,
+    label: "blog",
+    milestone: "Introduced the timeline view",
+    link: "/blog/introducing-the-timeline-view",
+  },
+  {
+    timestamp: 1606923112654,
+    label: "project",
+    milestone: "10 active users of Your Analytics",
+    cta: "Check out Your Analytics",
+    link: "https://www.your-analytics.org",
+  },
+  {
+    timestamp: 1606923102654,
+    label: "blog",
+    milestone: "Why I moved from React to Svelte and you should too",
+    link: "/blog/introducing-the-timeline-view",
+  },
+  {
+    timestamp: 1606921112654,
+    label: "twitter",
+    milestone: "705 Followers 🎉",
+    cta: "Check out my tweets",
+    link: "/blog/introducing-the-timeline-view",
+  },
+  {
+    timestamp: 1577059200000,
+    label: "twitter",
+    milestone: "700 Followers 🎉",
+    cta: "Check out my tweets",
+    link: "/blog/introducing-the-timeline-view",
+  },
+  {
+    timestamp: 1577059100000,
+    label: "twitter",
+    milestone: "699 Followers 🎉",
+    cta: "Check out my tweets",
+    link: "/blog/introducing-the-timeline-view",
+  },
+  {
+    timestamp: 1526083200000,
+    label: "youtube",
+    milestone: "20 Subscribers 🎉",
+    cta: "Check out my videos",
+    link: "https://www.youtube.com",
+  },
+];
