6aceb8b6e90748499bfc976e7eec594061a032a6
Mike Nikles <mikenikles@gmail.com>
2020-12-05 23:36:03 +0000

    Add the footer.

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Footer from "../components/footer.svelte";
   import SeoHead from "../components/seo/head.svelte";
   import Subscribe from "../components/subscribe.svelte";
   import Timeline from "../components/timeline/index.svelte";
@@ -7,3 +8,4 @@
 <SeoHead />
 <Timeline />
 <Subscribe />
+<Footer />
