9e2a22ee0442b8a0783d0620788eb5cdba6c8c0a
Mike Nikles <mikenikles@gmail.com>
2020-12-06 00:51:14 +0000

    Add smooth scrolling to the filters section from the mobile bouncing down arrow.

--- services/website/src/components/timeline/filters.svelte
+++ services/website/src/components/timeline/filters.svelte
@@ -2,7 +2,13 @@
   import { filteredFilters, selectedFilters, allFiltersForDisplay } from "../../stores/timeline";
 </script>
 
-<div class="mt-10 px-2 grid grid-cols-2 gap-4 md:mt-20 md:mb-10 md:flex md:justify-center md:space-x-20">
+<style>
+  #filters {
+    scroll-margin-top: 4em;
+  }
+</style>
+
+<div id="filters" class="mt-10 px-2 grid grid-cols-2 gap-4 md:mt-20 md:mb-10 md:flex md:justify-center md:space-x-20">
   {#each $allFiltersForDisplay as filter}
     <div class="relative flex items-center text-{filter}">
       <div class="flex items-center h-6 md:h-10">
--- services/website/src/components/timeline/intro.svelte
+++ services/website/src/components/timeline/intro.svelte
@@ -20,9 +20,11 @@
           <p>Software Architect 👷. Productivity ⏱️ & Team Morale 😊. Web, Javascript, Cloud Native ☁️. Author 📚 and Educator 🧑‍🏫.</p>
           <p class="mt-5">I talk about software architecture, web development, cloud, productivity, team morale and what's next in tech.</p>
           <div class="mt-5 flex justify-center sm:hidden">
-            <svg class="animate-bounce w-6 h-6 text-blue-600" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-            </svg>
+            <a href="#filters">
+              <svg class="animate-bounce w-6 h-6 text-blue-600" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
