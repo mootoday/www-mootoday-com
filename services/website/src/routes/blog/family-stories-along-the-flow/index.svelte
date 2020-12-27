e4a1a0f3108c98488530792824309b96636cd4de
cliff <cliff.wong@ehlacademy.org>
2020-12-27 18:35:57 +0800

    add blog post

---
+++ services/website/src/routes/blog/family-stories-along-the-flow/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-12-18-family-stories-along-the-flow/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
---
+++ services/website/src/routes/blog/停止顯影後-需要一個冷靜的咆哮-鄭多莉個人展覽-a-calm-rant-is-needed-after-the-stop-bath-a-dory-cheng-solo/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2020-12-27-停止顯影後-需要一個冷靜的咆哮-鄭多莉個人展覽-a-calm-rant-is-needed-after-the-stop-bath-a-dory-cheng-solo/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
