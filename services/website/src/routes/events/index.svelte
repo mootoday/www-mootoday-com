70eacf4c89b055b23e0d6eeee6cce2547d420dcd
cliff <cliff.wong@ehlacademy.org>
2020-12-27 18:20:16 +0800

    event list page

--- services/website/src/routes/events/index.svelte
+++ services/website/src/routes/events/index.svelte
@@ -21,6 +21,22 @@
 <div style="padding-top: 60px;">
 	<h1>events</h1>
 </div>
-{#each events as e}
-	{e.metadata.title}
+{#each events as post}
+	<a href="blog/{post.metadata.slug}" class="flex">
+		<div style="min-width: 12em; max-width: 12em;">
+			<img
+							src={getCover(post)}
+							loading="lazy"
+							alt="Cover picture for a blog post titled {post.metadata.title}" />
+		</div>
+		<div class="fw ml10">
+			<h4>{post.metadata.title}</h4>
+			<p class="mt5 text-gray-700">{post.metadata.summary}</p>
+			<p class="mt5">
+				{#each post.metadata.tags as t}
+					<a href="/teatime/tags/{t}" class="mx3 text-gray-600">#{t}</a>
+				{/each}
+			</p>
+		</div>
+	</a>
 {/each}
