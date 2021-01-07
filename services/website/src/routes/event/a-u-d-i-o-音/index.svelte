bb5fbdb3abc431a530378213c682e490acce60c4
cliff <cliff.wong@ehlacademy.org>
2021-01-07 19:14:20 +0800

    add assets

---
+++ services/website/src/routes/event/a-u-d-i-o-音/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2021-01-07-a-u-d-i-o-音/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
