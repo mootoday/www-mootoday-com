33158ba84f3c979c511a2c7e963989c28127e267
cliff <cliff.wong@ehlacademy.org>
2021-01-07 20:57:29 +0800

    project

--- services/website/src/routes/index.svelte
+++ services/website/src/routes/index.svelte
@@ -45,6 +45,21 @@
   	const {event_start_date, event_end_date} = event.metadata
 	  return '' + formatDate(event_start_date) + "-" + formatDate(event_end_date)
   }
+
+  const news = [
+	  {
+	  	date: '31-12-2020',
+		  body: '藝發局資助《平地數碼》錄像文章網上雜誌第一期 12月31日刊登'
+	  },
+	  {
+		  date: '01-03-2020',
+		  body: '藝發局資助《平地數碼》錄像文章網上雜誌第2期公開徵稿 3月1日截止'
+	  },
+	  {
+		  date: '31-03-2021',
+		  body: '藝發局資助《平地數碼》錄像文章網上雜誌第2期 3月31日上載'
+	  }
+  ]
 </script>
 
 <SeoHead />
@@ -77,6 +92,50 @@
 						<p class="text-sm">{getExcerpt(post)}</p>
 					</a>
 				{/each}
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container mx-auto">
+	<div class="grid grid-cols-2 gap-4">
+		<div>
+			<p class="font-bold text-xs">重點項目：</p>
+			<h2 class="text-4xl mb-2 text-pink-800 font-bold">平地數碼</h2>
+			<p>《平地數碼》有三個重點。（1）以平易近人的「錄像文章」為焦點，建立一個具包容性的群體，（2）鼓勵發表，善用現成的媒體和個別的資源，開創新的表述語言，（3）於可發展、再用、延伸的在線虛擬環境，集合展館與存庫的功用，持續更生。</p>
+			<p>More</p>
+		</div>
+		<div>
+			<h3>News:</h3>
+			{#each news as n}
+				<div class="flex items-center">
+					<div class="flex-shrink-0 w-16 text-center">
+						<div class="text-2xl">
+							JAN
+						</div>
+						<div class="text-sm">
+							2021
+						</div>
+					</div>
+					<p class="p-4">{n.body}</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="grid grid-cols-2 gap-4">
+		<div>
+			<h2>原格破裂 – 動畫的互媒實驗綜覽</h2>
+			<p>源於一股脈衝，又或是在諸多提問的裂縫中想去尋蹤覓跡的驅策，《原格破裂 – 動畫的互媒實驗綜覽》的研習以社會文化歷史與技術譜系的宏闊景觀作願景去研習動畫的端倪。</p>
+		</div>
+		<div>
+			<div>
+				series 01
+			</div>
+			<div>
+				series 02
 			</div>
 		</div>
 	</div>
