ffd6e68fb4f1a9db1a06c5c098a373af61d3731a
Mike Nikles <mikenikles@pm.me>
2022-12-12 01:57:10 +0000

    Add active nav item indicator.

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -207,9 +207,16 @@
                     {#each navigationItems as {href, label}}
                       <li>
                         <a
-                          class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                          {href}>{label}</a
-                        >
+                          class:text-teal-500={$page.url.pathname.startsWith(href)}
+                          class:dark:text-teal-400={$page.url.pathname.startsWith(href)}
+                          class:hover:text-teal-500={!$page.url.pathname.startsWith(href)}
+                          class:dark:hover:text-teal-400={!$page.url.pathname.startsWith(href)}
+                          class="relative block px-3 py-2 transition"
+                          {href}>{label}
+                          {#if $page.url.pathname.startsWith(href)}
+                            <span class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>
+                          {/if}
+                        </a>
                       </li>
                     {/each}
                   </ul>
