b1ba1ea51a70d19fc02635430c8363111a431932
Mike Nikles <mikenikles@pm.me>
2022-12-19 03:53:39 +0000

    Only show top 3 blog posts on the homepage.

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,15 +1,19 @@
 import type { PageLoad } from './$types';
 
+const topArticleSlugs = [
+	'dev-environments-in-the-cloud-are-a-half-baked-solution',
+	'svelte-kit-prisma-a-match-made-in-digital-heaven',
+	'why-i-moved-from-react-to-svelte-and-why-others-will-follow'
+];
+
 export const load: PageLoad = async () => {
-	const mdModules = import.meta.glob('../content/blog/**/index.md');
-	const posts = (await Promise.all(
-		Object.keys(mdModules)
-				.map(async (path) => {
-				const slug = path.split('/').at(-2);
-				const { metadata } = await mdModules[path]();
-				return { slug, ...metadata };
-			})
-	))
-	.sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
+	const topModules = topArticleSlugs.map((slug) => import(`../content/blog/${slug}/index.md`));
+	const result = await Promise.all(topModules);
+	const posts = result
+		.map((page, index) => ({
+			slug: topArticleSlugs[index],
+			...page.metadata
+		}))
+		.sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
 	return { postsTotal: posts.length, posts: posts.slice(0, 3) };
 };
