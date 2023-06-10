b12f1140457298db9b7313622e70b0fe448ac640
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-06-10 04:44:41 +0000

    Move devbox config to separate directory.

---
+++ .config/devbox/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+check_port() {
+  port=$1
+  if lsof -i :$port | grep LISTEN > /dev/null; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if ! check_port 5173; then
+    npm install
+    npm run dev
+fi
