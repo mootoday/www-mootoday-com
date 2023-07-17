657105edb8acf0e86ecdf0248821d7f68fd91674
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-07-17 09:35:20 -0700

    Add more logging

--- src/routes/feed/+page.server.ts
+++ src/routes/feed/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({platform}) => {
     console.log(platform);
     console.log(platform?.env);
-    console.log(platform?.env.MIKENIKLES_COM_FEED);
+    console.log(JSON.stringify(platform?.env.MIKENIKLES_COM_FEED));
     const entries = platform?.env.MIKENIKLES_COM_FEED.list() || [];
     console.log({entries});
     return {
