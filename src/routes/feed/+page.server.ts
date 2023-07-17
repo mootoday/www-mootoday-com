d47d4f4047f2c1fc8bbdb126d832d61ad016cbfa
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-07-17 07:08:59 -0700

    Print entries on the sever

--- src/routes/feed/+page.server.ts
+++ src/routes/feed/+page.server.ts
@@ -2,6 +2,7 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({platform}) => {
     const entries = platform?.env.MIKENIKLES_COM_FEED.list() || [];
+    console.log({entries});
     return {
         entries
     };
