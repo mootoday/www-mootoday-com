1f99c9939039dc683ad53c99e2a81816858b728f
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-07-17 22:43:03 -0700

    Hide like & comment icons for now

--- src/lib/components/feed/entry.svelte
+++ src/lib/components/feed/entry.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import SvelteMarkdown from "svelte-markdown";
+	import SvelteMarkdown from 'svelte-markdown';
 
 	export let entry: {
 		id: string;
@@ -79,51 +79,51 @@
 				alt="An emergency exit light on a call"
 			/>
 		</div> -->
-		<div class="flex">
-		<div class="w-full">
-			<div class="flex items-center">
-				<div class="text-center">
-					<a
-						href="#"
-						class="group mt-1 flex w-12 items-center rounded-full px-3 py-2 text-base font-medium leading-6 text-gray-500 hover:bg-blue-800 hover:text-blue-300"
-					>
-						<svg
-							class="h-6 w-6 text-center"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							><path
-								d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-							/></svg
+		<!-- <div class="flex">
+			<div class="w-full">
+				<div class="flex items-center">
+					<div class="text-center">
+						<a
+							href="#"
+							class="group mt-1 flex w-12 items-center rounded-full px-3 py-2 text-base font-medium leading-6 text-gray-500 hover:bg-teal-800 hover:text-teal-300"
 						>
-					</a>
-				</div>
+							<svg
+								class="h-6 w-6 text-center"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								><path
+									d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+								/></svg
+							>
+						</a>
+					</div>
 
-				<div class="m-2 py-2 text-center">
-					<a
-						href="#"
-						class="group mt-1 flex w-12 items-center rounded-full px-3 py-2 text-base font-medium leading-6 text-gray-500 hover:bg-blue-800 hover:text-blue-300"
-					>
-						<svg
-							class="h-7 w-6 text-center"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							><path
-								d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-							/></svg
+					<div class="m-2 py-2 text-center">
+						<a
+							href="#"
+							class="group mt-1 flex w-12 items-center rounded-full px-3 py-2 text-base font-medium leading-6 text-gray-500 hover:bg-teal-800 hover:text-teal-300"
 						>
-					</a>
+							<svg
+								class="h-7 w-6 text-center"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								><path
+									d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+								/></svg
+							>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>
+		</div> -->
 	</div>
 	<hr class="border-gray-600 mt-4" />
 </div>
