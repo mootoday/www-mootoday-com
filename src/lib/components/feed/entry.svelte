0e4719807dc0f56c7e90d6dabaefbe8395f4d06f
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-07-20 05:22:51 -0700

    Use heroicons

--- src/lib/components/feed/entry.svelte
+++ src/lib/components/feed/entry.svelte
@@ -153,16 +153,9 @@
 							on:click={share}
 							class="w-12 mt-1 group flex items-center text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full hover:bg-teal-800 hover:text-teal-300"
 						>
-							<svg
-								class="text-center h-7 w-6"
-								fill="none"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" /></svg
-							>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-7">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+							</svg>							
 						</button>
 					</div>
 				</div>
